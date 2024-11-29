90a9624c94eb48bbef34d90c856d2eeeefae7694
Lucky Shrestha <luckystha92@gmail..com>
2024-11-29 20:26:31 +0545

    Backend committed

---
+++ src/books/book.route.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const Book = require('./book.model');
+const { postABook, getAllBooks, getSingleBook, UpdateBook, deleteABook } = require('./book.controller');
+const verifyAdminToken = require('../middleware/verifyAdminToken');
+const router = express.Router();
+
+//frontend => backend server => controller => book schema => database => send to server => Back to the frontend
+//post a book
+//post = when submit something frontend to db 
+//get = when get something back from db
+//put/patch = when edit or update something
+//delete = when delete something
+
+
+//post a book
+router.post("/create-book", verifyAdminToken, postABook);
+
+//get all books
+router.get("/", getAllBooks);
+
+// single books endpoint
+router.get("/:id", getSingleBook);
+
+//update a book endpoint
+router.put("/edit/:id",verifyAdminToken, UpdateBook);
+
+
+router.delete("/:id",verifyAdminToken, deleteABook)
+
+module.exports = router;
